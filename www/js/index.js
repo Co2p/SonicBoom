cac8cd2ddf460bc7350fd49034bea02cbe6d9c7c
Gordon Cooper <go.co2p@gmail.com>
2016-10-14 15:09:06 +0200

    Initial UI test

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,43 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var colours = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'yellow black-text', 'amber black-text', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'];
+var lightness = ['darken-4 waves-light', 'darken-3 waves-light', 'darken-2 waves-light', 'darken-1 waves-light', 'waves-light', 'lighten-1 waves-light', 'lighten-2 black-text', 'lighten-3 black-text', 'lighten-4 black-text', 'lighten-5 black-text'];
+function randomColour() {
+    return colours[Math.floor((Math.random() * colours.length))] + ' ' + lightness[Math.floor((Math.random() * lightness.length))]
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+function seqColour(int1, int2) {
+    while (int1 >= colours.length) {
+        int1 = int1 - colours.length;
     }
-};
+    while (int2 >= lightness.length) {
+        int2 = int2 - lightness.length;
+    }
+    return colours[int1] + ' ' + lightness[int2]
+}
 
-app.initialize();
+function keyClick(row, id) {
+    $('"#c' + id + 'r' + row + '"').text(latestKey);
+}
+
+function keyRow(keyCount, rowId) {
+    let html = '<div class="row">';
+    const width = (100 / keyCount) - 0.5;
+    for (let i = 0; i < keyCount; i++) {
+        const button = $('<button id="c' + i + 'r' + rowId +'" onclick="keyClick(' + i + ',' + rowId + ').bind(this)" class="waves-effect btn ' + seqColour(rowId, i) + '" style="width:' + width + '%; margin-left: 0.25%; margin-right:0.25%"></button>').text(i);
+        html += button[0].outerHTML;
+    }
+    html += '</div>';
+    return html;
+}
+
+let html = '<div>';
+
+html += keyRow(13, 1);
+html += keyRow(14, 2);
+html += keyRow(14, 3);
+html += keyRow(13, 4);
+html += keyRow(14, 5);
+html += keyRow(8, 6);
+
+html += '</div>';
+
+$(".buttons").append(html);
