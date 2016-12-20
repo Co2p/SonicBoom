87569103141805391e71e225dec4a3a657f5a8c9
Co2p <go.co2p@gmail.com>
2016-12-20 12:16:07 +0100

    Working audiobuffer load and play

--- www/js/audio.js
+++ www/js/audio.js
@@ -1,13 +1,13 @@
 // Source: https://www.html5rocks.com/en/tutorials/webaudio/intro/
-var context;
-var audioBuffers = new Hash(); //Hashtable with buffers
+var audioContext = new AudioContext();
+var audioBuffers = {}; //Hashtable with buffers
 window.addEventListener('load', initAudio, false);
 
 function initAudio() {
   try {
     // Fix up for prefixing
     window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    context = new AudioContext();
+    audioContext = new AudioContext();
     console.log('audio init done');
   }
   catch(e) {
@@ -16,23 +16,35 @@ function initAudio() {
 }
 
 function playSound(id) {
-  var source = context.createBufferSource(); // creates a sound source
+  if (audioBuffers[id] != undefined) {
+    playSound(id, 0);
+  } else {
+    //TODO wait for audio to be defined + timeout
+  }
+}
+
+function playSound(id, time) {
+  console.log("playing id: " + id);
+  var source = audioContext.createBufferSource(); // creates a sound source
+  console.log(audioBuffers[id]);
   source.buffer = audioBuffers[id];                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
-                                             // note: on older systems, may have to use deprecated noteOn(time);
+  source.connect(audioContext.destination);       // connect the source to the context's destination (the speakers)
+  source.start(time);                           // play the source now from tile ( 0 )
+  // note: on older systems, may have to use deprecated noteOn(time);
 }
 
 function loadSound(url, id) {
+  console.log("loading id: " + id);
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
 
   // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
+  request.onload = () => {
+    audioContext.decodeAudioData(request.response, function(buffer) {
       audioBuffers[id] = buffer;
-    }, onError);
+      console.log("loaded " + id);
+    }, () => {console.log("oups, thats a audio error");});
   }
   request.send();
 }
