7d8301f1aabb6080c151f2c06706103942548733
Gordon Cooper <go.co2p@gmail.com>
2018-09-07 13:33:59 +0200

    First steps towards a dynamic keyboard layout

--- www/js/UI.js
+++ www/js/UI.js
@@ -1,49 +1,36 @@
+"use-strict";
+var  keys = {};
 function initUI() {
- load('keylayout/SE_mac.json', function(response) {
-    var jsonLayout = JSON.parse(response);
-    $('.keyboard').append(buildUI(jsonLayout));
- });
-}
-
-function buildUI(jsonLayout) {
-  var keytable = '<div>';
-  for(i in jsonLayout.rows) {
-    let row = jsonLayout.rows[i];
-    let tableRow = '<div>';
-    for(j in row) {
-      tableRow += buildButton(row[j]);
+    // $.getJSON('keylayout/SE_mac.json', function(json) {
+    //     var jsonLayout = JSON.parse(response);
+    //     $('.keyboard').append(buildUI(jsonLayout));
+    //     console.log(jsonLayout);
+    // });
+    let keyboard = document.getElementsByClassName('keyboard')[0];
+    for (var i = 0; i < 6; i++) {
+        let div = document.createElement("div");
+        div.id = 'keyrow' + (i + 1)
+        div.className = 'keyrow';
+        keyboard.appendChild(div);
     }
-    tableRow += '</div>';
-    keytable += tableRow;
-  }
-  keytable += '</div>'
-  return keytable
+
 }
 
-function buildButton(key) {
-  let className = "mdl-cell mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect button-key";
-  let prop = '';
-  if (key.class != 'undefined') {
-    className += " " + key.class;
-  }
-  if (key.prop === 'disabled') {
-    //prop += " " + key.prop;
-    className += " mdl-button--colored mdl-color--red";
-  }
-  return '<button class="' + className + '" id="' + key.ID + '" ' + prop + '>' + key.name + '<p class="description">' + 'description' + '</p></button>';
+function addButton(keyID, row) {
+    let className = "mdl-cell mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect button-key";
+
+    let key = keys[keyID];
+
+    document.getElementById('keyrow' + row).innerHTML += '<button class="' + className + '" id="' + key.name + '">' + key.label + '<p class="description">' + 'description' + '</p></button>';
 }
 
-
-function load(file, callback) {
-
-   var xobj = new XMLHttpRequest();
-       xobj.overrideMimeType("application/json");
-   xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
-   xobj.onreadystatechange = function () {
-         if (xobj.readyState == 4 && xobj.status == "200") {
-           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-           callback(xobj.responseText);
-         }
-   };
-   xobj.send(null);
+function key(event, sound) {
+    console.log(event);
+    this.name = event.keyCode;
+    this.label = event.key;
+    this.class = '';
+    loadSound(sound, name);
+    this.play = function () {
+        playSound(name)
+    }
 }
--- www/js/keylistener.js
+++ www/js/keylistener.js
@@ -1,18 +1,27 @@
 let previousKey;
+let keyRow = 1;
+
 document.onkeyup=function(e){
-  //TODO make things happen, also cooperate with keydown
-  console.log(e.code);
-  previousKey = '';
+    //TODO make things happen, also cooperate with keydown
+    console.log(e.code);
+    previousKey = '';
 }
 
 document.onkeydown=function(e) {
-  if (previousKey == e.code) {
-    return;
-  } else {
-    loadSound('ide2.wav', e.code);
-    playSound(e.code);
-    //TODO make things happen
-    console.log(e.code);
-    previousKey = e.code;
-  }
+    if (previousKey == e.keyCode) {
+        return;
+    } else {
+        previousKey = e.keyCode;
+        if (!keys[previousKey]) {
+            let newKey = new key(e, 'ide2.wav');
+            keys[previousKey] =  newKey;
+            addButton(previousKey, keyRow)
+        } else {
+            keyRow++;
+        }
+        loadSound('ide2.wav', previousKey);
+        playSound(previousKey);
+        //TODO make things happen
+        console.log(previousKey);
+    }
 }
